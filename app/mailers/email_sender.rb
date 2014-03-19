75b71bae2627cf80ed42787fea8bb8065271a9ac
MichelleGlauser <michelleglauser@gmail.com>
2014-03-19 20:50:53 +0800

    Removed unnecessary item in mailer.

--- app/mailers/email_sender.rb
+++ app/mailers/email_sender.rb
@@ -3,7 +3,6 @@ class EmailSender < ActionMailer::Base
 
     def email_message(email)
   		@email = email
-  		@address = email.address
   		mail(:to => email.address, :message => "This is your message from Michelle using Ruby on Rails (via mailer)." )
   	end
 end
