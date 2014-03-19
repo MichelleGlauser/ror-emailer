d1c75fd51695c6f459006bf56c707424b1c2ec06
MichelleGlauser <michelleglauser@gmail.com>
2014-03-19 18:19:30 +0800

    Initial commit for ROR emailer.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emailer::Application.config.secret_token = 'd481d0f8980085790af78f4deffcbc17ec820e8f4f25c838312abbcb71671255726f0dcd921b3a0d5b5e7b42a981e8a0e05909da4ce36fd5c7887a2db28466d9'
